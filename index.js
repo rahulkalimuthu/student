d14352138842ff2908864cf3778d06f44cb2229b
rahulkalimuthu <31270899+rahulkalimuthu@users.noreply.github.com>
2024-06-26 00:18:37 +0530

    express project

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import userRoutes from './routes/users.js'
+
+const app = express();
+const port = 4000;
+app.use(bodyParser.json())
+
+app.use('/users', userRoutes);
+
+// app.get('/', (req, res) => {
+//     console.log('[GET ROUTE]');
+//     res.send('HELLO FROM HOMEPAGE');
+// })
+
+app.get('/', (req, res));
+
+app.listen(port, () => console.log(`server running on port : http://localhost:${port}`))
