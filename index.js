9de52b57872ee64b91d1247c0669eafadeb6088c
rahulkalimuthu <31270899+rahulkalimuthu@users.noreply.github.com>
2024-06-26 00:23:30 +0530

    users routes included

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json())
 
 app.use('/users', userRoutes);
 
-// app.get('/', (req, res) => {
-//     console.log('[GET ROUTE]');
-//     res.send('HELLO FROM HOMEPAGE');
-// })
+app.get('/', (req, res) => {
+    console.log('[GET ROUTE]');
+    res.send('HELLO FROM HOMEPAGE');
+})
 
-app.get('/', (req, res));
+// app.get('/', (req, res));
 
 app.listen(port, () => console.log(`server running on port : http://localhost:${port}`))
